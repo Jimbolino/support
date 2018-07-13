109d1fb46da3364416ddfa6b934c2a563a992690
MattyRad <mradford@protonmail.com>
2018-07-12 17:28:10 -0700

    Remove PHP 5.6 from test suite, not worth the trouble

--- tests/Unit/Sample.php
+++ tests/Unit/Sample.php
@@ -2,92 +2,46 @@
 
 use MattyRad\Support\Conformation;
 
-if (PHP_VERSION_ID > 70100) {
-    class Sample
-    {
-        use Conformation;
+class Sample
+{
+    use Conformation;
 
-        private $array;
-        private $str;
-        private $int;
-        private $bool;
-        private $float;
-        private $optional1;
-        private $optional2;
+    private $array;
+    private $str;
+    private $int;
+    private $bool;
+    private $float;
+    private $optional1;
+    private $optional2;
 
-        private function __construct(
-            array $array,
-            string $str,
-            int $int,
-            bool $bool,
-            ?float $float,
-            $optional1 = '',
-            \stdClass $optional2 = null
-        ) {
-            $this->array = $array;
-            $this->str = $str;
-            $this->int = $int;
-            $this->bool = $bool;
-            $this->float = $float;
-            $this->optional1 = $optional1;
-            $this->optional2 = $optional2;
-        }
-
-        public function toArray()
-        {
-            return [
-                'array' => $this->array,
-                'str' => $this->str,
-                'int' => $this->int,
-                'bool' => $this->bool,
-                'float' => $this->float,
-                'optional1' => $this->optional1,
-                'optional2' => $this->optional2,
-            ];
-        }
+    private function __construct(
+        array $array,
+        string $str,
+        int $int,
+        bool $bool,
+        ?float $float,
+        $optional1 = '',
+        \stdClass $optional2 = null
+    ) {
+        $this->array = $array;
+        $this->str = $str;
+        $this->int = $int;
+        $this->bool = $bool;
+        $this->float = $float;
+        $this->optional1 = $optional1;
+        $this->optional2 = $optional2;
     }
-} else {
-    class Sample
+
+    public function toArray()
     {
-        use Conformation;
-
-        private $array;
-        private $str;
-        private $int;
-        private $bool;
-        private $float;
-        private $optional1;
-        private $optional2;
-
-        private function __construct(
-            array $array,
-            $str,
-            $int,
-            $bool,
-            $float,
-            $optional1 = '',
-            \stdClass $optional2 = null
-        ) {
-            $this->array = $array;
-            $this->str = $str;
-            $this->int = $int;
-            $this->bool = $bool;
-            $this->float = $float;
-            $this->optional1 = $optional1;
-            $this->optional2 = $optional2;
-        }
-
-        public function toArray()
-        {
-            return [
-                'array' => $this->array,
-                'str' => $this->str,
-                'int' => $this->int,
-                'bool' => $this->bool,
-                'float' => $this->float,
-                'optional1' => $this->optional1,
-                'optional2' => $this->optional2,
-            ];
-        }
+        return [
+            'array' => $this->array,
+            'str' => $this->str,
+            'int' => $this->int,
+            'bool' => $this->bool,
+            'float' => $this->float,
+            'optional1' => $this->optional1,
+            'optional2' => $this->optional2,
+        ];
     }
 }
